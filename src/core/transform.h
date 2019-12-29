41b84ead1b6ece75d77400f67b628135590b62e8
Wei Li <viclw17@gmail.com>
2019-12-29 01:57:13 -0800

    edit

--- src/core/transform.h
+++ src/core/transform.h
@@ -51,12 +51,15 @@ struct Matrix4x4 {
     // Matrix4x4 Public Methods
     Matrix4x4() {
         m[0][0] = m[1][1] = m[2][2] = m[3][3] = 1.f;
-        m[0][1] = m[0][2] = m[0][3] = m[1][0] = m[1][2] = m[1][3] = m[2][0] =
-            m[2][1] = m[2][3] = m[3][0] = m[3][1] = m[3][2] = 0.f;
+        m[0][1] = m[0][2] = m[0][3] = 
+        m[1][0] = m[1][2] = m[1][3] = 
+        m[2][0] = m[2][1] = m[2][3] = 
+        m[3][0] = m[3][1] = m[3][2] = 0.f;
     }
     Matrix4x4(Float mat[4][4]);
-    Matrix4x4(Float t00, Float t01, Float t02, Float t03, Float t10, Float t11,
-              Float t12, Float t13, Float t20, Float t21, Float t22, Float t23,
+    Matrix4x4(Float t00, Float t01, Float t02, Float t03, 
+              Float t10, Float t11, Float t12, Float t13, 
+              Float t20, Float t21, Float t22, Float t23,
               Float t30, Float t31, Float t32, Float t33);
     bool operator==(const Matrix4x4 &m2) const {
         for (int i = 0; i < 4; ++i)
@@ -70,7 +73,9 @@ struct Matrix4x4 {
                 if (m[i][j] != m2.m[i][j]) return true;
         return false;
     }
+
     friend Matrix4x4 Transpose(const Matrix4x4 &);
+
     void Print(FILE *f) const {
         fprintf(f, "[ ");
         for (int i = 0; i < 4; ++i) {
@@ -83,6 +88,7 @@ struct Matrix4x4 {
         }
         fprintf(f, " ] ");
     }
+    
     static Matrix4x4 Mul(const Matrix4x4 &m1, const Matrix4x4 &m2) {
         Matrix4x4 r;
         for (int i = 0; i < 4; ++i)
